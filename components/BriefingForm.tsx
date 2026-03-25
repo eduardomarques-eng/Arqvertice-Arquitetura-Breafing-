85687018393d05bc88761a881d81725227a8109c
Eduardo Marques <eduardo.marques.arq@gmail.com>
2026-03-25 11:45:37 -0300

    Change export from default to named export for BriefingForm

--- components/BriefingForm.tsx
+++ components/BriefingForm.tsx
@@ -1,40 +1,9 @@
-'use client';
-
 import React from 'react';
-import { useForm } from 'react-hook-form';
 
-const BriefingForm = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-
-    const onSubmit = (data) => {
-        console.log(data);
-    };
-
+export function BriefingForm() {
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor="nome">Nome</label>
-                <input id="nome" {...register('nome', { required: true })} />
-                {errors.nome && <span>This field is required</span>}
-            </div>
-            <div>
-                <label htmlFor="telefone">Telefone</label>
-                <input id="telefone" {...register('telefone', { required: true })} />
-                {errors.telefone && <span>This field is required</span>}
-            </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input id="email" {...register('email', { required: true })} />
-                {errors.email && <span>This field is required</span>}
-            </div>
-            <div>
-                <label htmlFor="cidadeEstado">Cidade/Estado</label>
-                <input id="cidadeEstado" {...register('cidadeEstado', { required: true })} />
-                {errors.cidadeEstado && <span>This field is required</span>}
-            </div>
-            <button type="submit">Submit</button>
+        <form>
+            {/* Form structure goes here */}
         </form>
     );
-};
-
-export default BriefingForm;
+}
